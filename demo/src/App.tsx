d1d7fcde37d1e4228d4fa7e0e0e1f6d87acdf56f
Ravindra Sadaphule <rsadaphule@gmail.com>
2024-11-09 18:04:33 -0800

    added data type

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -3,21 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let var1: [string, number] = ["Hello", 124] 
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          The variable {var1} is of the type {typeof(var1)} data type.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
