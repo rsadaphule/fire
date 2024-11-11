6b05a5f7a1e97ca04c9fedf3f2bba9751e359a9c
Ravindra Sadaphule <rsadaphule@gmail.com>
2024-11-10 20:21:28 -0800

    Added timer example

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Message from './Message';
 
@@ -7,12 +7,28 @@ const App: React.FC = () =>
 {
    const [userName, setUserName] = useState<string>('User');
    const [userMessage, setUserMessage] = useState<string>('This is the initial message');
+   const [timeLeft, setTimeLeft] = useState(5);
+
+
+   useEffect(() => {
+
+    if (timeLeft === 0) return;
+
+    const timerId = setTimeout(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+
+    return () => clearTimeout(timerId);
+  
+   }, [timeLeft])
+
  
   return (
     <>
       <h1>Typescript is cool</h1>
       <div>
          <Message name={userName} message={userMessage} ></Message>
+         <h4>Countdown: {timeLeft}</h4>
       </div>
     </>
   )
